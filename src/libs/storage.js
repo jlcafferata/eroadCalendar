6b0bae501c54b4d82dd35f48a8ac51426d4f9c9c
Jose Luis <jlcafferata@gmail.com>
2018-11-18 11:00:57 -0300

    Clear function and try errors - JLC

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -1,4 +1,8 @@
 export default {
+  clear: function(){
+    localStorage.clear();
+  },
+
   get: function(k) {
     try {
       return JSON.parse(localStorage.getItem(k));
@@ -34,17 +38,20 @@ export default {
       itemsFound.push(JSON.stringify(this.get(year+element)));
     });
     
-    itemsFound.forEach(element => {
-      var itemJSON=JSON.parse(element);
-      var monthCalendar=itemJSON.calendar[0];
-      monthCalendar.days.forEach(element => {
-        if(element.category!=='none'){
-          var count=((itemsJSON[element.category])?itemsJSON[element.category]:0);
-          itemsJSON[element.category]=count+1;
-        }
+    try{
+      itemsFound.forEach(element => {
+        var itemJSON=JSON.parse(element);
+        var monthCalendar=itemJSON.calendar[0];
+        monthCalendar.days.forEach(element => {
+          if(element.category!=='none'){
+            var count=((itemsJSON[element.category])?itemsJSON[element.category]:0);
+            itemsJSON[element.category]=count+1;
+          }
+        });      
       });
-      
-    });
+    } catch(error){
+      //
+    }
     return itemsJSON;
   }
 };
